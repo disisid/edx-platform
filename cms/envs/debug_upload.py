51decf85ea0c22615f5e38fa8efa265ff8a870cc
Sarina Canelake <sarina@edx.org>
2014-11-30 01:57:48 -0500

    s/pylint: disable=W0401/pylint: disable=wildcard-import/

--- cms/envs/debug_upload.py
+++ cms/envs/debug_upload.py
@@ -1,5 +1,5 @@
-# pylint: disable=W0614, W0401
-from .dev import *
+# pylint: disable=W0614
+from .dev import *  # pylint: disable=wildcard-import
 
 FILE_UPLOAD_HANDLERS = (
     'contentstore.debug_file_uploader.DebugFileUploader',
